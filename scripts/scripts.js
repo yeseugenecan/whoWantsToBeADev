c535fd499f589297108873357c191d3367cac92d
Eugene Michasiw <eugene@michasiw.com>
2019-05-22 18:46:13 -0400

    array issue

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,6 +20,62 @@
 
 // if user selects a wrong answer, then show the correct answer and display game over and their final score.
 
+const app = {};
+
+app.level = 0;
+app.questions = [];
+app.correctAnswers = [];
+app.incorrectAnswers = [];
+
+app.getQuestions = (difficulty) => {
+    return $.ajax({
+        url: `https://opentdb.com/api.php?amount=5&category=18&difficulty=${difficulty}&type=multiple`,
+        method: `GET`,
+        dataType: `json`
+    });
+}
+
+async function getData() {
+    const data = await app.getQuestions('medium');
+    const arrayOfQuestions = data.results;
+    arrayOfQuestions.forEach((arrayItem) => {
+        app.questions.push(arrayItem.question);
+        app.correctAnswers.push(arrayItem.correct_answer);
+        app.incorrectAnswers.push(arrayItem.incorrect_answers)
+    })
+}
+console.log(app.questions[0], app.correctAnswers, app.incorrectAnswers)
+getData();
+
+app.randomizeAnswers = (correct, wrongAnswer) => {
+    // const allAnswers = wrongAnswer.concat(correct);
+    console.log(correct, wrongAnswer)
+    // return shuffle(allAnswers);
+}
+
+// from https://bost.ocks.org/mike/shuffle/
+app.shuffle = (array) => {
+    let m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    return array;
+}
+
+console.log(app.randomizeAnswers(app.correctAnswers[0], app.incorrectAnswers[0]))
 
 
+app.init = () => {
+}
 
+
+$(function () {
+    app.init();
+
+});
