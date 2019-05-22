f53068fcc1549193ca222691364eee33e503eefc
Can Rozanes <canrozanes@gmail.com>
2019-05-22 16:50:23 -0400

    pseudocode added to JS file

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+// create an object called theGame
+// Make 3 different API calls to fetch 15 questions in varying difficulties.
+// Store these questions in an array of objects.
+
+
+// const displayQuestion = function (question) {
+//     correctAnswer = question.correct_answer;
+//     incorrectAnswer = question.incorrect_answers;
+
+//     const randomize = function (correctAnswer, incorrectAnswer) {
+//         //do stuff to randomnize
+
+//         return arrayOfRandomnizedAnswers
+//     }
+// }
+
+// When user clicks start, display the first question using the displayQuestion function shown above.
+
+// if user selects the right answer that matches the correctAnswer then show that they are correct and proceed to the next question
+
+// if user selects a wrong answer, then show the correct answer and display game over and their final score.
+
+
+
+
